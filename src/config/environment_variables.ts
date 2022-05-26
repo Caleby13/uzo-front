4f0e84664ee81b87dda7025aea99050ae83e6b5b
Caleby Almeida Soares <71778060+Caleby13@users.noreply.github.com>
2022-05-26 13:48:40 -0300

    Update environment_variables.ts

--- src/config/environment_variables.ts
+++ src/config/environment_variables.ts
@@ -1,4 +1,6 @@
-export const baseURL =
+export const baseURL ="https://uzoapi2.herokuapp.com"
+
+export const baseURLBackup =
   import.meta.env.MODE == "development"
     ? "https://uzoapi2.herokuapp.com"
     : import.meta.env.API_URL;
